e4e88e57400f040e1eea68a74b728c53210a5295
fernando <fernando@debian.tortrix.com>
2018-03-12 16:46:36 -0600

    comence a arreglar el segundo metodo

--- src/main/java/controlador/Archivo.java
+++ src/main/java/controlador/Archivo.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,23 +11,18 @@ private List<String> textoLista = new ArrayList<String>();
 
     public boolean validarPath(final String path){
         boolean salida = false;
-        BufferedReader br = null;
         //con trim eliminaremos los espacios que pueda contener el path
         if(path!=null && !path.trim().isEmpty()){
-            try{
-                br = new BufferedReader(new FileReader(path));
-            }catch(FileNotFoundException fe){
-                System.out.println(""+fe);
-            }
-            salida = true;
+            salida = Paths.get(path).toFile().exists();
         }
         return salida;
     }
     /* se agrego un segundo parametro String Caracter, porque asi sera mas dinamico el metodo a la hora 
     de llamar*/
     public List<String> cargarArchivo(final String path, String caracter){
-        validarPath(path);
-        // String csvFile = new File("").getAbsolutePath ()+"/src/main/resources/tpi2018.csv";
+        
+        if(validarPath(path)){
+             
         textoLista.removeAll(textoLista);
 
         BufferedReader br = null;
@@ -42,7 +38,7 @@ private List<String> textoLista = new ArrayList<String>();
                 for(int i=0; i<country.length;i++){
                      text = text+"\t"+country[i];
                 }
-                //String text = "Country [code= " + country[4] + " , name=" + country[5] + "]";
+                
                 System.out.println(text);
                 textoLista.add(text);
 
@@ -61,6 +57,8 @@ private List<String> textoLista = new ArrayList<String>();
                 }
             }
         }
-        return textoLista;
+       }
+       return textoLista;   
+     
     }
 }
